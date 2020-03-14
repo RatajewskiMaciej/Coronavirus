7dfd38985ddc0e42da4ec23e0ab6f4e5f9cb162d
RatajewskiMaciej <ratajewskimaciej@wp.pl>
2020-03-14 22:25:24 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
+import React, { useEffect } from 'react';
+import Navbar from '../components/Navbar';
+import LandingPage from '../components/LandingPage';
 import Calendar from '../components/Calendar';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
