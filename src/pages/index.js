965158ea1060a2a31bc60a22efc54259d15eb423
RatajewskiMaciej <ratajewskimaciej@wp.pl>
2020-03-14 22:23:38 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,19 @@
-import React, { useEffect } from 'react';
-import Navbar from '../components/Navbar';
-import LandingPage from '../components/LandingPage';
-import Calendar from '../components/Calendar'
+import Calendar from '../components/Calendar';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
-import CalendarProvider from '../components/calendarContext/provider'
+import { Provider } from 'react-redux'
+import store from '../components/redux/store'
 
 const index = () => {
   useEffect(() => {
     M.AutoInit();
   });
   return (
-    <div>
-      <CalendarProvider>
-        <Navbar />
-        <Calendar />
-        <LandingPage />
-      </CalendarProvider>
-    </div >
-  )
-}
-export default index
+    <Provider store={store}>
+      <Navbar />
+      <Calendar />
+      <LandingPage />
+    </Provider>
+  );
+};
+export default index;
